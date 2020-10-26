0ee002c81dcf1e8feebd2d54823fa196d240e33e
Yenfri29 <73499258+Yenfri29@users.noreply.github.com>
2020-10-26 16:28:25 -0500

    iniciando git
    Se inicia con el registro y el reporte

--- pythonProject3mvc_bd/Controller/PersonaController.py
+++ pythonProject3mvc_bd/Controller/PersonaController.py
@@ -5,27 +5,26 @@ class PersonaController:
     def __init__(self):
         pass
 
-    #********************** CREAR REGISTROS *************************
+    #******* CREAR REGISTROS *******
     def crearRegistro(self,lista):
         self.enlace=Alumno()
-        self.enlace.setDatospersonales(lista[0],lista[1],lista[2],lista[3])
+        self.enlace.setDatosPersonales(lista[0],lista[1],lista[2],lista[3])
         self.enlace.setMatricula(lista[4],lista[5])
-        self.enlace.insertarAlumno()
-        if (resp):
+        resp=self.enlace.insertarAlumno()
+        if(resp):
             print("registro correcto")
-          #  return 1
         else:
             print("registro incorrecto")
-          #  return 0
-    #********************** LEER REGISTROS *************************
+
+    #******* LEER REGISTROS *******
     def listarRegistros(self):
-        self.enlace =Alumno()
+        self.enlace=Alumno()
         lista=self.enlace.mostrarAlumnos()
+        return lista
 
-
-    #********************** ACTUALIZAR REGISTRO *************************
+    #******* ACTUALIZAR REGISTROS *******
     def actualizarRegistro(self,codigo):
         pass
-    #********************** ELIMINAR REGISTRO *************************
-    def eliminarRegistro(self):
+    #******* ELIMINAR REGISTROS *******
+    def eliminarRegistro(selfself,codigo):
         pass
--- pythonProject3mvc_bd/Model/Alumno.py
+++ pythonProject3mvc_bd/Model/Alumno.py
@@ -1,14 +1,13 @@
-from .Conexion import Conexion
 from .Persona import Persona
+from .Conexion import Conexion
 
 class Alumno(Persona):
     codigo=""
     ciclo=""
-
     def __init__(self):
         pass
 
-    def setDatospersonales(self,nombre,apellido,dni,sexo):
+    def setDatosPersonales(self,nombre,apellido,dni,sexo):
         Persona.__init__(self,nombre,apellido,dni,sexo)
 
     def setMatricula(self,codigo,ciclo):
@@ -21,34 +20,34 @@ class Alumno(Persona):
     def setCiclo(self,ciclo):
         self.ciclo=ciclo
 
-    def getCodigo(self):
-        return self.codigo
-
     def getCiclo(self):
         return self.ciclo
 
+    def getCodigo(self):
+        return self.codigo
+#**************************************************************
     def insertarAlumno(self):
         conecta=Conexion()
         conecta.conectar()
-        SQL="INSERT INTO alumno(codigo,nombres,apellidos,dni,sexo,ciclo) VALUES('"+self.codigo+"','"+self.nombre+"','"+self.apellido+"','"+self.dni+"','"+self.sexo+"',"+str(self.ciclo)+")"
+        SQL="INSERT INTO alumno(codigo,nombres,apellidos,dni,sexo,ciclo)" \
+            " VALUES('"+self.codigo+"','"+self.nombre+"','"+self.apellido+"','"+self.dni+"','"+self.sexo+"',"+str(self.ciclo)+")"
         print(SQL)
         resp=conecta.setEjecutarQuery(SQL)
-        if (resp):
-
+        if(resp):
             return 1
         else:
             return 0
-#******************************************************************
+#*************************************************************
     def mostrarAlumnos(self):
-        conecta = Conexion()
+        conecta=Conexion()
         conecta.conectar()
-        SQL ="SELECT *FROM alumno"
+        SQL="SELECT *FROM alumno"
         registros=conecta.getEjecutarQuery(SQL)
         return registros
-#******************************************************************
-    def actualizarAlumno(self,codigo):
+#***************************************************************
+    def actualizarAlumno(selfself,codigo):
         pass
 
-    def eliminarAlumno(self):
+    def eliminarAlumno(self,codigo):
         pass
 
